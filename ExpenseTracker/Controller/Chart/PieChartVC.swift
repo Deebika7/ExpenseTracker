67300a045c585097a945df70deee84337d0c647f
Deebika G <deebikagovind@gmail.com>
2023-05-10 10:21:20 +0530

    initial commit

--- ExpenseTracker/Controller/Chart/PieChartVC.swift
+++ ExpenseTracker/Controller/Chart/PieChartVC.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  PieChartVC.swift
 //  ExpenseTracker
 //
 //  Created by deebika-pt6680 on 09/05/23.
@@ -7,13 +7,15 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class PieChartVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .systemBackground
+        
     }
+    
 
+    
 
 }
-
---
+++ ExpenseTracker/Controller/MoneyTracker/Record/CategoriesListVC.swift
@@ -0,0 +1,21 @@
+//
+//  CategoriesListVC.swift
+//  ExpenseTracker
+//
+//  Created by deebika-pt6680 on 09/05/23.
+//
+
+import UIKit
+
+class CategoriesListVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+        
+    }
+    
+
+    
+
+}
